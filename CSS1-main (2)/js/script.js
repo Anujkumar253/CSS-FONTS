f9775534ca9ad8dfcf88f3cfd10482c4213704b1
Anujkumar253 <136267917+Anujkumar253@users.noreply.github.com>
2023-06-11 15:29:49 -0400

    Add files via upload

---
+++ CSS1-main (2)/js/script.js
@@ -0,0 +1,9 @@
+
+    function Lyrics() {
+      var lyrics = document.getElementById('lyrics');
+      if (lyrics.style.display === 'none') {
+        lyrics.style.display = 'block';
+      } else {
+        lyrics.style.display = 'none';
+      }
+    }
